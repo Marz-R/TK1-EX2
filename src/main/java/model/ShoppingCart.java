9ff021845f92b901e6a5a9777c880638aa76027e
Marz-R <roshiannacheung@gmail.com>
2021-12-08 05:16:27 +0100

    Updated ShoppingCart.java and ShoppingCartItem.java

--- src/main/java/model/ShoppingCart.java
+++ src/main/java/model/ShoppingCart.java
@@ -1,10 +1,42 @@
 package model;
 
+import java.util.*;
+
 public class ShoppingCart{
 	
 	private String clientId;
+	private List<ShoppingCartItem> items = new ArrayList<ShoppingCartItem>();
 	
 	public ShoppingCart(String clientId) {
 		this.clientId = clientId;
 	}
+	
+	public void addItem(String flightNum, String seatNum, String meal, int price) {
+		ShoppingCartItem sci = new ShoppingCartItem(flightNum, seatNum, meal, price);
+		
+		if (!sci.repeat(flightNum, items)) {
+			items.add(sci);
+		} else {
+			System.out.println("Client " + clientId + " booked a seat on this flight already");
+		}
+	}
+	
+	// calculate final price
+	public int totalPrice() {
+		int total = 0;
+		for (ShoppingCartItem sci : items) {
+			total += sci.getPrice();
+		}
+		return total;
+	}
+	
+	public List<ShoppingCartItem> getCartItem(){
+		return items;
+	}
+	
+	// every booked seat gets a unique booking id
+	public String getBookingId() {
+		return clientId + "BK" + items.size();
+	}
+	
 }
--- src/main/java/model/ShoppingCartItem.java
+++ src/main/java/model/ShoppingCartItem.java
@@ -1,7 +1,42 @@
 package model;
 
+import java.util.List;
+
 public class ShoppingCartItem{
-  public ShoppingCartItem(){
-    
-  }
+	
+	private String flightNum;
+	private String seatNum;
+	private String meal;
+	private int price;
+	
+	public ShoppingCartItem(String flightNum, String seatNum, String meal, int price){
+		this.flightNum = flightNum;
+		this.seatNum = seatNum;
+		this.meal = meal;
+		this.price = price;
+	}
+	
+	public String getFlightNum() {
+		return this.flightNum;
+	}
+	
+	public String getSeatNum() {
+		return this.seatNum;
+	}
+	
+	public String getMeal() {
+		return this.meal;
+	}
+	
+	public int getPrice() {
+		return this.price;
+	}
+	
+	// check if client book the same flight twice
+	public boolean repeat(String flightNum, List<ShoppingCartItem> items) {
+		for (ShoppingCartItem sci : items) {
+			if (sci.getFlightNum().equals(flightNum)) return true;
+		}
+		return false;
+	}
 }
